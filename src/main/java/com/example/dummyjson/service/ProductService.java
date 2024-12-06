1739ab41aa39de95181b2b22dde2403c16fd32db
Thiago Monteiro <thicmp@gmail.com>
2024-12-06 12:48:42 -0300

    1 - Injeção das dependências via construtor afim de aplicar boas práticas de programação. 2 - Métodos que utilizam RestTemplate foram marcados como depreciados e tiveram seus nomes sufixados para indicar isso. 3 - Passam a vigorar os métodos de busca que utilizam WebClient.

--- src/main/java/com/example/dummyjson/service/ProductService.java
+++ src/main/java/com/example/dummyjson/service/ProductService.java
@@ -1,13 +1,13 @@
 package com.example.dummyjson.service;
 
 import com.example.dummyjson.dto.Product;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.dummyjson.dto.ProductListResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,16 +15,41 @@ public class ProductService {
 
     private final String BASE_URL = "https://dummyjson.com/products";
 
-    @Autowired
+    private final WebClient webClient;
+
     private RestTemplate restTemplate;
 
-    public List<Product> getAllProducts() {
+    public ProductService(WebClient webClient, RestTemplate restTemplate) {
+        this.webClient = webClient;
+        this.restTemplate = restTemplate;
+    }
+
+    @Deprecated
+    public List<Product> getAllProductsDeprecated() {
         Product[] products = restTemplate.getForObject(BASE_URL, Product[].class);
         return Arrays.asList(products);
     }
 
-    public Product getProductById(Long id) {
+    @Deprecated
+    public Product getProductByIdDeprecated(Long id) {
         String url = BASE_URL + "/" + id;
         return restTemplate.getForObject(url, Product.class);
     }
+
+    public Mono<ProductListResponse> getAllProducts() {
+        return webClient
+                .get()
+                .uri("/products")
+                .retrieve()
+                .bodyToMono(ProductListResponse.class);
+    }
+
+    public Mono<Product> getProductById(Long id) {
+        return webClient
+                .get()
+                .uri("/products/" + id)
+                .retrieve()
+                .bodyToMono(Product.class);
+    }
+
 }
