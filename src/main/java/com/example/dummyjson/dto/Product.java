07a46115c6d794235553deacc06eb1f749c9458b
Thiago Monteiro <thicmp@gmail.com>
2024-12-06 14:11:50 -0300

    Adicionadas anotações que solicitam ao Lombok que gere construtor sem argumentos, com todos os argumentos e também aplique o design pattern Builder.

--- src/main/java/com/example/dummyjson/dto/Product.java
+++ src/main/java/com/example/dummyjson/dto/Product.java
@@ -3,13 +3,13 @@ package com.example.dummyjson.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @EqualsAndHashCode(of = {"id", "title"})
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Builder(builderClassName = "builder")
 public class Product {
     @NotNull
     @Min(0L)
