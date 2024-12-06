18684df182405cd6030f57c85e2c6b82c518cc39
Thiago Monteiro <thicmp@gmail.com>
2024-12-06 12:23:48 -0300

    Ajuste nos imports das contraints de Bean Validation e utilização do Lombok para melhorar a legibilidade do código.

--- src/main/java/com/example/dummyjson/dto/Product.java
+++ src/main/java/com/example/dummyjson/dto/Product.java
@@ -1,12 +1,16 @@
 package com.example.dummyjson.dto;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@EqualsAndHashCode(of = {"id", "title"})
+@NoArgsConstructor
 public class Product {
-
     @NotNull
     @Min(0L)
     @Max(999L)
@@ -20,38 +24,4 @@ public class Product {
 
     @NotNull
     private Double price;
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
