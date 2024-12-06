ab388fedf0857b356b82fffdcf963ee34781cd11
Thiago Monteiro <thicmp@gmail.com>
2024-12-06 14:34:39 -0300

    Ajuste no mapeamento do controller afim de reconhecer o novo context-path da aplicação.

--- src/main/java/com/example/dummyjson/controller/ProductController.java
+++ src/main/java/com/example/dummyjson/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
